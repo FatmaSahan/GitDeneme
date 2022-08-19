d60ce927ac47487a382087e700828410bc13cac4
fatma <ffsbm3842@gmail.com>
2022-08-19 11:17:13 +0100

    V2

--- src/C01_Git/C01_Git.java
+++ src/C01_Git/C01_Git.java
@@ -3,5 +3,6 @@ package C01_Git;
 public class C01_Git {
     public static void main(String[] args) {
         System.out.println("heloo githup");
+        System.out.println("2. deneme");
     }
 }
